017e0d239810fc0ec11313638f3942f6035a377e
rojedyashwanth-cloud <rojedyashwanth@gmail.com>
2026-01-01 08:17:14 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+    return(
+        <div className='app'>
+            <h1>Profile</h1>
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td>Rojed Yashwanth</td>
+                </tr>
+                <tr>
+                    <td>Section:</td>
+                    <td>S03</td>
+                </tr>
+                <tr>
+                    <td>Program:</td>
+                    <td>B-Tech CSE</td>
+                </tr>
+                <tr>
+                    <td>Course:</td>
+                    <td>FEDF</td>
+                </tr>
+            </table>
+        </div>
+            
+    );
+}
+
+export default App;
